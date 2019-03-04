08f9d4e7f8b9ba2954881592788a7c1a1dabc9ec
Serg <serg.ghazaryan@mail.ru>
2019-03-04 19:29:55 +0300

    a

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ const bot = new TelegramBot(TOKEN, {polling: true})
 bot.on('message', msg => {
     bot.sendMessage(msg.chat.id, `hello from heroku, bot says: "hi, ${msg.from.first_name}`)
 })
+
+bot.onText(/напомни (.+) в (.+)/, function (msg, match) {
+    var userId = msg.from.id
+    var text = match[1]
+    var time = match[2]
+
+    notes.push({ 'uid': userId, 'time': time, 'text': text })
+
+    bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)')
+})
+
